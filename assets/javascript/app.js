e868409a5ebcc94183e7092ff69d6e187cc67a85
John Lewis <jalewis1221@gmail.com>
2018-07-11 20:11:03 -0500

    finished project and added a favorites button that stores gif info in local storeage.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,8 +2,8 @@
 
 //initial array of gifs
 
-var wordList = ["surfing", "tuna", "pineapple", "dolphin", "aloha", "turtle", "reef", "fishing", "luau"];
-
+var wordList = ["surfing", "tuna", "pineapple", "dolphin", "aloha", "turtle", "reef", "fishing", "luau","poop","rainbow", "surf board", "spear-fishing"];
+var count = 0;
 //function for displaying string value buttons
 
 $("#add-gif").on("click", function(event) {
@@ -18,10 +18,63 @@ $("#add-gif").on("click", function(event) {
       console.log(wordList);
       renderButtons();
  });
+            //you can use this es6 function arrow unless you are using the $this feature
+
+            //extract out the info in a funtion so it can build out the image
+    $("#favorite").on('click',() => {
+      
+        
+        $("#display").css("display", "unset");
+        $("#display").empty();
+        let gifs = JSON.parse(localStorage.getItem('gifs'));
+        if (gifs){ 
+            gifs.forEach(gif => {
+                
+                console.log(gif.still);
+
+                let div = $("<div>").addClass("pic");
+                let p = $("<p>").html("Rating: "+ gif.rating+"<br>"+"Title: " +gif.title);
+                
+
+                var image = $("<img>")
+                        .attr("src", gif.still)
+                        .attr("data-url", gif.original);
+                   
+                    div.append(p).append(image);
+
+                    
+                    $("#display").append(div);
+
+                   
 
 
 
 
+            });
+
+        } else {
+            alert("you have not picked a favorite!")
+        }
+
+        $("img").on('click', function(){
+
+            console.log(this);
+            let gif = $(this);
+            let nextURL = gif.attr("data-url");
+            
+            gif.attr("data-url", gif.attr("src"));
+            gif.attr("src", nextURL);
+          
+        });
+
+    });
+
+$("#clear").on('click', () =>{
+        localStorage.clear();
+        $("#display").empty();
+        count = 0;
+        $("#count").html(count);
+});
 
 
 
@@ -59,25 +112,60 @@ function renderButtons(){
                 console.log(response);
                 $("#display").empty();
                 for (let i = 0; i < response.data.length; i++) {
-                    var div = $("<div>").addClass("pic");
-                    var p = $("<p>").html("Rating: "+ response.data[i].rating+"<br>"+" Title: " +response.data[i].title);
+                    let div = $("<div>").addClass("pic");
+                    let { title, rating, images } = response.data[i];
+                    let p = $("<p>").html("Rating: "+ response.data[i].rating+"<br>"+"Title: " +response.data[i].title);
+                    let fav =$("<p>").html("Add Favorite").addClass("icon");
+                    
+                    fav.on('click', function() {
+                            count ++;
+                            $("#count").html(count);
+                            
+                            const gifData = localStorage.getItem('gifs');
+                            let gifs;
+
+                            if (gifData) {
+                                console.log(gifData)
+                                gifs = JSON.parse(gifData);
+                            } else {
+                                gifs = [];
+                            }
+
+                            const data = {
+                                title,
+                                rating,
+                                still: images.fixed_height_still.url,
+                                original: images.fixed_height.url,
+                            }
+
+                            gifs.push(data);
+
+                            localStorage.setItem('gifs', JSON.stringify(gifs));
+                            
+
+
+
+
+                    });
+
                    
                       
                     var image = $("<img>")
                         .attr("src", response.data[i].images.fixed_height_still.url)
                         .attr("data-url", response.data[i].images.fixed_height.url);
-                    div.append(p).append(image);
+                    div.append(p).append(fav).append(image);
 
 
                     
                     $("#display").append(div);
                 }
+                //if I use image for the icon be sure not to use the same onclick listener
                 $("img").on('click', function(){
                     console.log("hello");
                     let gif = $(this)
                     let nextURL = gif.attr("data-url");
                     
-                    gif.attr("data-url", gif.attr("src"))
+                    gif.attr("data-url", gif.attr("src"));
                     gif.attr("src", nextURL);
             
             
